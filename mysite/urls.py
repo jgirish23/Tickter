9a8b354718803f9b7bf551df047dc08c7821c2b9
jgirish23 <jakkalagirish@gmail.com>
2024-03-21 22:55:42 +0530

    made / as login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.signup, name="login"),
     path("admin/", admin.site.urls),
     path("index/",views.index, name="index"),
     path("login/",views.signup, name="login"),
